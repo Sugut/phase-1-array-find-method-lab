3c63f6e078e37d2c21e9760d33c9b4eb86d33967
Sugut <sugutvictorine@gmail.com>
2022-08-20 23:05:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record,year){
+    let found = record.find(({result})=>result==="W");
+    return found ? found['year']:undefined;
+}
+
+superbowlWin.find();
